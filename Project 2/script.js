c06602859e721fc0987599e9d5b51ad75bc77aac
can0020 <can02@risd.edu>
2021-11-05 02:51:19 -0400

    save

---
+++ Project 2/script.js
@@ -0,0 +1,43 @@
+// var weather = ["cloud.png", "sun.png", "rain.png", "snow.png"];
+
+var weather = ["cloud.png", "sun.png", "rain.png", "snow.png"];
+
+function displayImg(){
+    var num = Math.floor(Math.random() * (weather.length));
+    document.canvas.src="img/"+weather[num];
+}
+
+// let img = document.getElementById("weather");
+
+// function showWeather(num) {
+// 	let num = Math.floor(Math.random()*4);
+// 	document.getElementById("weather").style.backgroundImage = "url('weather[num]')";
+// }
+
+// showWeather();
+// ---------------------
+// var weatherList = new Array("weather/cloud.png", "weather/rain.png","weather/snow.png", "weather/sun.png");
+
+// function showWeather() {
+//      var randomWeather = Math.floor(Math.random() * weatherList.length);
+//      document.getElementById("randomWeather").src = weather[randomWeather];
+
+// image_array = [
+// 	'sun.png',
+// 	'cloud.png',
+// 	'rain.png',
+// 	'snow.png'
+// ]
+
+// function showWeather() {
+// 	random_index = Math.floor(Math.random() * image_array.length);
+// 	selectedImage = image_array[random_index];
+// 	document.getElementById('image_shower').src = './images/$[selectedImage]'
+// }
+
+// var imagesArray = ["sun.png","cloud.png","rain.png", "snow.png"];
+
+// function displayWeather() {
+//     var num = Math.floor(Math.random() * 4);
+//     document.canvas.src = imagesArray[num];
+// }
